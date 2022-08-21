2210b991c2e863d1dd7c2eb2dcd6a53d5876aa6a
Daria Smirnova <darya.smirn1@yandex.ru>
2022-08-21 16:03:09 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { HeaderMenu } from './components/HeaderMenu/HeaderMenu';
+import "antd/dist/antd.css";
+import { Layout } from 'antd';
+import { Top250MoviesList } from './pages/top250/Top250MoviesList';
+
 
 function App() {
+  const { Content } = Layout
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <HeaderMenu />
+      <Content>
+        <Routes>
+          <Route path='/' element={<Navigate to='/top250-movies' />} />
+          <Route path='/top250-movies' element={<Top250MoviesList />} />
+          <Route path='/most-popular-movies' />
+          <Route path='/random-movie' />
+          <Route path='/favorite-movies' />
+          <Route path='/search-movie' />
+          <Route path='*' element={'404'} />
+        </Routes>
+      </Content>
+    </Layout>
   );
 }
 
