08274fc96822c0bc7700b70c6b21f905dcf0c4bc
Daria Smirnova <darya.smirn1@yandex.ru>
2022-10-04 20:15:23 +0300

    add styles to MovieItem component
    Changed the display of the interaction button with favorites
    Add different click handlers

--- src/components/MovieItem/MovieItem.tsx
+++ src/components/MovieItem/MovieItem.tsx
@@ -1,24 +1,26 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-import { MovieType } from "../../redux/top250Slice/top250Slice"
+import { DeleteFilled, PlusCircleFilled } from '@ant-design/icons';
 import styles from './MovieItem.module.scss'
 import { Button } from 'antd';
+import { MovieType } from '../../redux/moviesSlice/moviesSlice';
 
-export const MovieItem: FC<PropsType> = ({ movie, handleClick }) => {
+export const MovieItem: FC<PropsType> = ({ movie, addToFavorite, removeFromFavorite }) => {
 	return (
 		<div className={styles.movie}>
 			<img src={movie.image} alt={movie.title} loading='lazy' />
 			<div className={styles.actionsBlock}>
-				<Link to={`${movie.id}`} className={styles.moreLink}>More</Link>
-				<Button type="default" onClick={() => handleClick(movie.id)}>
-					{movie.isLiked ? 'Remove from Favorite' : 'Add to Favorite'}
-				</Button>
+				<Link to={`/movie/${movie.id}`} className={styles.moreLink}>More</Link>
+				{movie.isLiked
+					? <Button onClick={() => removeFromFavorite(movie.id)} className={styles.favoriteBtn} title='Удалить из избранного'><DeleteFilled /></Button>
+					: <Button onClick={() => addToFavorite(movie)} className={styles.favoriteBtn} title='Добавить в избранное'><PlusCircleFilled /></Button>}
 			</div>
-		</div>
+		</div >
 	)
 }
 
 type PropsType = {
 	movie: MovieType
-	handleClick: (id: string) => void
+	addToFavorite: (movie: MovieType) => void
+	removeFromFavorite: (id: string) => void
 }
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,17 +1,18 @@
 import { FC } from "react"
-import { MovieType } from "../../redux/top250Slice/top250Slice"
+import { MovieType } from "../../redux/moviesSlice/moviesSlice"
 import { MovieItem } from "../MovieItem/MovieItem"
 import styles from './MoviesList.module.scss'
 
 export const MoviesList: FC<PropsType> = (props) => {
 	return (
 		<div className={styles.moviesContainer}>
-			{props.movies.map(m => <MovieItem key={m.id} movie={m} handleClick={props.handleClick} />)}
+			{props.movies.map(m => <MovieItem key={m.id} movie={m} addToFavorite={props.addToFavorite} removeFromFavorite={props.removeFromFavorite} />)}
 		</div>
 	)
 }
 
 type PropsType = {
 	movies: MovieType[]
-	handleClick: (id: string) => void
+	addToFavorite: (movie: MovieType) => void
+	removeFromFavorite: (id: string) => void
 }
