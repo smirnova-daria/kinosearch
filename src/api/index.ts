2c5b458734c5a99223403bb49838c8695c8059e5
Daria Smirnova <darya.smirn1@yandex.ru>
2022-10-05 22:01:40 +0300

    add function for fetching movie by id

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-
+//import mockMovie from '../mock/mockMovie.json'
 const instance = axios.create({
 	baseURL: 'https://imdb-api.com/ru/API/',
 })
@@ -8,6 +8,10 @@ const apikey = "pk_haswrb539erohnlhc"
 export const moviesAPI = {
 	fetchMoviesList(type: string): Promise<ResponseType> {
 		return instance.get(`${type}/${apikey}`).then(res => res.data)
+	},
+	fetchMovieById(id: string): Promise<ResponseMovieType> {
+		return instance.get(`Title/${apikey}/${id}/FullActor,Images,Trailer`).then(res => res.data)
+		//return mockMovie
 	}
 }
 
@@ -24,7 +28,32 @@ export type MoviesType = {
 	imDbRatingCount: string
 }
 
-export type ResponseType = {
+type ResponseType = {
 	items: MoviesType[],
 	errorMessage: string
+}
+
+export type ResponseMovieType = {
+	id: string
+	title: string
+	year: string
+	image: string
+	runtimeStr: string
+	plot: string
+	directorList: ListItemType[]
+	writerList: ListItemType[]
+	starList: ListItemType[]
+	actorList: ListItemType[]
+	genreList: { key: string, value: string }[]
+	companyList: ListItemType[]
+	countryList: { key: string, value: string }[]
+	imDbRating: string
+	images: { items: { image: string }[] }[]
+	trailer: { linkEmbed: string }
+	similars: { id: string, title: string, image: string, imDbRating: string }[]
+}
+
+type ListItemType = {
+	id: string
+	name: string
 }
